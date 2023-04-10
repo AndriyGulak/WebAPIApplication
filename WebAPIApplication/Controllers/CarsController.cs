675a57e71c05c90f626d2aec413e28b798232072
Andrii.Gulak <Andriy.Gulak@gmail.com>
2023-04-10 22:04:45 +0300

    Add Cars Model +Controller

---
+++ WebAPIApplication/Controllers/CarsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPIApplication.Model;
+
+namespace WebAPIApplication.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class CarsController : ControllerBase
+    {
+        private readonly ILogger<CarsController> _logger;
+        public CarsController(ILogger<CarsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetCars")]
+        public IEnumerable<Cars> Get()
+        {
+            return Cars.GenCars().ToArray();
+        }
+
+    }
+
+}
---
+++ WebAPIApplication/Model/Cars.cs
@@ -0,0 +1,22 @@
+﻿namespace WebAPIApplication.Model
+{
+    public class Cars
+    {
+        public string VIN { get; set; }
+        public int Year { get; set; }
+        public string Maker { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public double Price { get; set; }
+
+        public static List<Cars> GenCars()
+        {
+            var cars = new List<Cars>();
+            cars.Add(new Cars { VIN = "VBNM234RP76", Year = 2011, Maker = "BMW", Model = "328i", Color = "White", Price = 20123.20D });
+            cars.Add(new Cars { VIN = "UYU7589HHF001", Year = 2019, Maker = "Kia", Model = "Soul", Color = "Green", Price = 16563.00D });
+            cars.Add(new Cars { VIN = "LKMNTRF1569745", Year = 2022, Maker = "Ford", Model = "Mustang", Color = "Yellow", Price = 40569.02D });
+            cars.Add(new Cars { VIN = "WDJGU846SHFY002", Year = 2017, Maker = "Mazda", Model = "CX-5", Color = "Gray", Price = 28550.47D });
+            return cars;
+        }
+    }
+}
