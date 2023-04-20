162ee89fc69ad51d2213e9ce1e3a72d699bd0f3d
Andrii.Gulak <Andriy.Gulak@gmail.com>
2023-04-20 17:46:35 +0300

    HW3 Delegates

---
+++ DelegatesHW3/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace DelegatesHW3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Print printString = PrintGreen;
+            printString.Invoke("Hello, World!");
+            printString = PrintOrange;
+            printString("Hello, World!");
+
+            Action<string> printAction = new Action<string>(PrintGreen);
+            printAction("Action Green");
+
+            printAction = PrintOrange;
+            printAction("Action Orange");
+
+            //PrintGreen("Hello, World!");
+            //PrintOrange("Hello, World!");
+        }
+        public delegate void Print(string msg);
+
+        static public void PrintGreen(string msg)
+        { 
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(msg);
+            Console.ResetColor();
+        }
+
+        static public void PrintOrange(string msg)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine(msg);
+            Console.ResetColor();
+        }
+    }
+}
--- WebApplicationHW/Services/ManagementCars.cs
+++ WebApplicationHW/Services/ManagementCars.cs
@@ -13,12 +13,10 @@ namespace WebApplicationHW.Services
         public string GetCarAge() => (DateTime.Now.Year - Car.Year).ToString();
         public string GetCarEngine() => Car.Engine; //" GetCarEngine()";//Car.Engine;
         public string GetCarName() => Car.Maker + " " + Car.Model;
-
         public List<Car> GetCars() => Cars;
         public ManagementCars()
         {
             Car = new Car { VIN = "VBNM234RP76", Year = 2011, Maker = " BMW", Model = "328i", Color = "White", Price = 20123.20D, Engine = "Petrol" };
-
 
             Cars = new List<Car>();
             Cars.Add(new Car { VIN = "VBNM234RP76", Year = 2011, Maker = "BMW", Model = "328i", Color = "White", Price = 20123.20D, Engine = "Petrol" });
